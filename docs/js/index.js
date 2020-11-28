c4d7c90d369c6d7746851008074df7706369a51a
Joaquín Buendía <jgamer669@gmail.com>
2020-11-28 13:47:51 -0500

    JS - changePrice

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,11 +2,34 @@
 
 var switch__bar = document.getElementsByClassName('switch__bar')[0];
 var button = switch__bar.querySelector('.switch__circle');
+var prices = document.querySelectorAll('.stats__amount');
+var monthlyPrices = ['19.99', '24.99', '39.99'];
+var annuallyPrices = ['199.99', '249.99', '399.99'];
 
-var toogleStyles = function toogleStyles() {
-  button.classList.toggle('active__circle');
+var changePrice = function changePrice() {
+  console.log(prices[0]);
+
+  if (cont == 0) {
+    for (var i = 0; i < prices.length; i++) {
+      prices[i].textContent = annuallyPrices[i];
+    }
+
+    cont++;
+  } else if (cont > 0) {
+    for (var _i = 0; _i < prices.length; _i++) {
+      prices[_i].textContent = monthlyPrices[_i];
+    }
+
+    cont--;
+  }
 };
 
+var cont = 0;
 button.addEventListener('click', function () {
-  toogleStyles();
+  positionButton();
+  changePrice();
 });
+
+var positionButton = function positionButton() {
+  button.classList.toggle('active__circle');
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,34 @@
 const switch__bar = document.getElementsByClassName('switch__bar')[0]
 const button = switch__bar.querySelector('.switch__circle')
 
-const toogleStyles = ()=>{
-    button.classList.toggle('active__circle')
+
+const prices = document.querySelectorAll('.stats__amount')
+
+const monthlyPrices = ['19.99','24.99','39.99']
+const annuallyPrices = ['199.99','249.99','399.99']
+
+const changePrice = ()=>{
+    console.log(prices[0])
+    if(cont == 0){
+        for(let i = 0; i<prices.length;i++){
+            prices[i].textContent = annuallyPrices[i]
+        }
+        cont++
+    }else if(cont > 0){
+        for(let i = 0; i<prices.length;i++){
+            prices[i].textContent = monthlyPrices[i]
+        }
+        cont--
+    }
 }
 
+let cont = 0
+
 button.addEventListener('click',()=>{
-    toogleStyles()
+    positionButton()
+    changePrice()
 })
+
+const positionButton = ()=>{
+    button.classList.toggle('active__circle')
+}
